c2ed44dba7911383a21a923edb9149d0e245b8b6
Hsiang-Fu Yu <rofu.yu@gmail.com>
2019-03-15 01:46:38 -0700

    update download script with curl

--- python/exp-scripts/datasets/download-data.sh
+++ python/exp-scripts/datasets/download-data.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 
-function download-data() {
-    CONFIRM=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate "https://docs.google.com/uc?export=download&id=$1" -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')
-    wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$CONFIRM&id=$1" -O $2
-    rm -rf /tmp/cookies.txt
+function gdrive-get() {
+    fileid=$1
+    filename=$2
+    if [[ "${fileid}" == "" || "${filename}" == "" ]]; then
+        echo "gdrive-curl gdrive-url|gdrive-fileid filename"
+        return 1
+    else
+        if [[ ${fileid} = http* ]]; then
+            fileid=$(echo ${fileid} | sed "s/http.*drive.google.com.*id=\([^&]*\).*/\1/")
+        fi
+        echo "Download ${filename} from google drive with id ${fileid}..."
+        cookie="/tmp/cookies.txt"
+        curl -c ${cookie} -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null
+        confirmid=$(awk '/download/ {print $NF}' ${cookie})
+        curl -Lb ${cookie} "https://drive.google.com/uc?export=download&confirm=${confirmid}&id=${fileid}" -o ${filename}
+        rm -rf ${cookie}
+        return 0
+    fi
 }
 
-download-data 1uJDqW4u2shjbA_Y27oIcq-QM2jgW4c9g electricity.npy
-download-data 1R_M3RP-t5CKDj5gUVTU2IEZXDInQKHJG traffic.npy
+gdrive-get 1uJDqW4u2shjbA_Y27oIcq-QM2jgW4c9g electricity.npy
+gdrive-get 1R_M3RP-t5CKDj5gUVTU2IEZXDInQKHJG traffic.npy
